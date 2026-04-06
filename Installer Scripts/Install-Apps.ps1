df22218ceb785c4b6b82d70cf940c2512ccdd0b2
Erick Roen <erick.roen@avanade.com>
2026-04-06 13:58:43 -0500

    Making some changes

---
+++ Installer Scripts/Install-Apps.ps1
@@ -0,0 +1,8 @@
+winget install voidtools.Everything --source winget
+winget install --id Git.Git -e --source winget
+winget install Microsoft.PowerShell --source winget
+winget install Microsoft.WindowsTerminal --source winget
+winget install Microsoft.VisualStudioCode --source winget
+winget install Microsoft.VisualStudio.Professional --source winget
+winget install Anthropic.ClaudeCode --source winget
+winget install Ollama.Ollama --source winget
--- Installer Scripts/Install-Winget.ps1
+++ Installer Scripts/Install-Winget.ps1
@@ -222,3 +222,12 @@ if ($removeMsStoreAsSource.IsPresent) {
     # Automatically accept source agreements to avoid prompts. Mostly applies to msstore.
     winget list --accept-source-agreements | Out-Null
 }
+
+
+winget install voidtools.Everything --source winget
+winget install --id Git.Git -e --source winget
+winget install Microsoft.PowerShell --source winget
+winget install Microsoft.WindowsTerminal --source winget
+winget install Anthropic.ClaudeCode --source winget
+winget install Microsoft.VisualStudioCode --source winget
+winget install Ollama.Ollama --source winget
